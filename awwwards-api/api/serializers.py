b8b6bf9ee1c36ad1353c10de6e2f12aa35809adb
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-10-26 15:45:49 +0300

    defined projectserializer

--- awwwards-api/api/serializers.py
+++ awwwards-api/api/serializers.py
@@ -6,3 +6,9 @@ class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
         fields = ['design', 'usability', 'content']
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['title', 'image', 'description', 'url', 'posted', 'rating']
