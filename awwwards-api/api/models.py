a615c76465f55261fa2124db41db19d9dd432246
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-10-26 14:24:27 +0300

    defined project model

--- awwwards-api/api/models.py
+++ awwwards-api/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='img/')
+    description = models.TextField
+    url = models.CharField()
+    posted = models.DateTimeField(auto_now_add=True)
