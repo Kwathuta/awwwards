e93fdb06bb1775630e81f99fe25d7b1b364b91a5
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-10-26 15:03:53 +0300

    bringing postgresql on board

--- awwwards-api/awwwards/settings.py
+++ awwwards-api/awwwards/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'awwwards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awwwards',
+        'USER': 'moringa',
+        'PASSWORD': 'aljokela7247',
     }
 }
 
