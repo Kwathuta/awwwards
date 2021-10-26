a1cf59f39b020f03c91c42226484dcfb24e79248
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-10-26 15:15:51 +0300

    initial migration

--- awwwards-api/api/models.py
+++ awwwards-api/api/models.py
@@ -4,16 +4,16 @@ from django.db import models
 
 
 class Rating(models.Model):
-    design = models.IntegerField(max_length=2)
-    usability = models.IntegerField(max_length=2)
-    content = models.IntegerField(max_length=2)
+    design = models.IntegerField
+    usability = models.IntegerField
+    content = models.IntegerField
 
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='img/')
     description = models.TextField(max_length=300)
-    url = models.CharField()
+    url = models.CharField(max_length=300)
     posted = models.DateTimeField(auto_now_add=True)
     rating = models.ForeignKey(Rating, on_delete=models.CASCADE)
 
