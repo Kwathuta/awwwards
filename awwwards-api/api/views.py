2b13ed8e2c63810bcacea0bb5a810ad16a521360
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-10-26 16:01:22 +0300

    adding project model views

--- awwwards-api/api/views.py
+++ awwwards-api/api/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from api.models import *
+from api.serializers import *
 
 # Create your views here.
+
+
+@csrf_exempt
+def projects_list(request):
+    """
+    List all projects, or create a new project.
+    """
+    if request.method == 'GET':
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ProjectSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def project_detail(request, pk):
+    """
+    Retrieve, update or delete a project.
+    """
+    try:
+        project = Project.objects.get(pk=pk)
+    except Project.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ProjectSerializer(project)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ProjectSerializer(project, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        project.delete()
+        return HttpResponse(status=204)
