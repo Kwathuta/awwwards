ce861a1097eda7c178f18556b8c50c48d3a18d19
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-10-29 15:20:18 +0300

    adding angular project

---
+++ awwwards-ui/awwwards/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavComponent } from './components/nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProjectsComponent,
+    NavbarComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
